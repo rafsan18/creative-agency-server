9b6d8338fef11417b6e34e13df70ff089d8ef67d
rafsan <rafsanamin18@gmail.com>
2020-10-16 00:28:54 +0600

    img upload loading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static("serviceImage"));
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -26,7 +29,20 @@ client.connect((err) => {
         .db("creativeAgency")
         .collection("services");
 
-    console.log("Db connected successfully");
+    app.post("/addAService", (req, res) => {
+        const image = req.files.image;
+        const title = req.body.title;
+        const description = req.body.description;
+        image.mv(`${__dirname}/serviceImage/${image.name}`, (err) => {
+            if (err) {
+                console.log(err);
+                return res
+                    .status(500)
+                    .send({ msg: "Failed to upload image in the server." });
+            }
+            return res.send({ name: image.name, path: `/${image.name}` });
+        });
+    });
 });
 
 app.listen(process.env.PORT || port);
